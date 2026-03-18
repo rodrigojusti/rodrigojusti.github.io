9ed931baa06d2b82e0d12eb4f54b3d53944e563d
rodrigojusti <36175464+rodrigojusti@users.noreply.github.com>
2026-03-18 09:10:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/rodrigojusti.github.io/
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
