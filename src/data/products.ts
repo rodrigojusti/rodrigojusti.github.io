a547c6386c2e2152fa243eb9625d0d911443bd79
Rodrigo Justi <rodrigo.cido@gmail.com>
2026-03-19 12:27:22 -0300

    inseridos itens e fotos - tentativa 4

--- src/data/products.ts
+++ src/data/products.ts
@@ -6,7 +6,7 @@ export const products: Product[] = [
     type: "agendas",
     desc: "Capa dura plasticada estilo caquinhos, interior com desenhos para colorir em papel 120g.",
     price: "R$ 1",
-    image: "/refs/heads/main/static/agenda_colorir_infantil.png",
+    image: "https://raw.githubusercontent.com/rodrigojusti/rodrigojusti.github.io/refs/heads/main/static/agenda_colorir_infantil.png",
     tags: ["infantil", "tamanho A5"],
   },
   {
@@ -14,7 +14,7 @@ export const products: Product[] = [
     type: "cartas",
     desc: "Capa com papel liso, personalizada com nome e interior com papel pautado para escrita de texto manuscrito, com decorações florais",
     price: "R$ 1",
-    image: "/refs/heads/main/static/bloco_cartas.png",
+    image: "https://raw.githubusercontent.com/rodrigojusti/rodrigojusti.github.io/refs/heads/main/static/bloco_cartas.png",
     tags: ["adulto", "tamanho AA", "testemunho"],
   },
   {
@@ -22,7 +22,7 @@ export const products: Product[] = [
     type: "cartas",
     desc: "Capa dura plastificada, personalizada com nome e interior com papel 75g com informações auxiliares para testemunho",
     price: "R$ 1",
-    image: "/refs/heads/main/static/agenda_pregacao_a6.png",
+    image: "https://raw.githubusercontent.com/rodrigojusti/rodrigojusti.github.io/refs/heads/main/static/agenda_pregacao_a6.png",
     tags: ["adulto", "tamanho A6", "testemunho"],
   },
   
